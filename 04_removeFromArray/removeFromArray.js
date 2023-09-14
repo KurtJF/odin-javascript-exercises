f8bca0cda546b97c2644555dbfeaac2c3c982fdd
kurt <kurttfrancisco@gmail.com>
2023-09-15 05:05:26 +0800

    Finished 1-6 of odin javascript exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (nums, ...args) {
+  // filter to iterate over array
+  return nums.filter((num) => !args.includes(num));
+  // for each num, check if it's not (!) included in args using the !args.includes(num)
+  // if condition is true, element is included in the result
+  // otherwise, it's filtered out.
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  // checks if (a, b) is a 'number' and is not negative
+  if (typeof a !== "number" || typeof b !== "number" || a < 0 || b < 0)
+    return "ERROR";
 
+  // Math.min return smallest number between (a, b)
+  // Math.max return largest number between (a, b)
+  let min = Math.min(a, b);
+  let max = Math.max(a, b);
+  let sum = 0;
+
+  // iterate over each number from a to b
+  for (let i = min; i <= max; i++) {
+    // sums each iterated number
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
