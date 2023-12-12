aa334f9436c0c4f3251bb98018f6cb2a2647bdc2
kurt <kurttfrancisco@gmail.com>
2023-12-13 07:45:07 +0800

    finished exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  return [...string]
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "")
+    .split("")
+    .reverse()
+    .join("");
 };
 
 // Do not edit below this line
