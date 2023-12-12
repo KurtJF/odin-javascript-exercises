57ea5f6f519c4c838549b4f0b35a1c36718db8d7
kurt <kurttfrancisco@gmail.com>
2023-12-12 11:02:07 +0800

    finished exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (num) {
+  return num.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (num) {
+  return num.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (a, b) {
+  return;
 };
 
 // Do not edit below this line
@@ -29,5 +29,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
