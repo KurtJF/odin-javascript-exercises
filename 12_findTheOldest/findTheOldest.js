90402314115f0face6310f299259913e67a0d55c
kurt <kurttfrancisco@gmail.com>
2023-12-13 10:11:05 +0800

    finished exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  return arr.reduce(old, (current) => {
+    const oldAge = getAge(old.yearOfBirth, old.yearOfDeath);
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
 
+    return oldAge < currentAge ? current : old;
+  });
 };
 
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 // Do not edit below this line
 module.exports = findTheOldest;
