bcfbadff8120b1ba2abf0c5daa691dff4aefd7bc
kurt <kurttfrancisco@gmail.com>
2023-12-13 08:59:31 +0800

    finished exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  if (num === 0) return 0;
 
+  let numOne = 1;
+  let numTwo = 0;
+
+  for (let i = 2; i <= num; i++) {
+    let currentNum = numOne + numTwo;
+    numTwo = numOne;
+    numOne = currentNum;
+  }
+
+  return numOne;
 };
 
 // Do not edit below this line
